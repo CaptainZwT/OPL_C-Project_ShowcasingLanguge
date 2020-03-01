1e67fd2949cf0a156d829fd20479b5a16865cf3c
Jesse Hines <jessehines@southern.edu>
2020-02-29 20:22:06 -0500

    TruLibrary Test

---
+++ Tru/Tests/TruLibraryTest.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using Tru;
+using Utility;
+
+namespace Tests
+{
+    public class TruLibraryTest
+    {
+        [Test]
+        public void TestBuiltIns() {
+            Assert.That( TruExpr.Parse("{nand true true}").Interpret(),   Is.EqualTo(new TruBool(false)));
+            Assert.That( TruExpr.Parse("{and  true true}").Interpret(),   Is.EqualTo(new TruBool(true)));
+            Assert.That( TruExpr.Parse("{or   false false}").Interpret(), Is.EqualTo(new TruBool(false)));
+            Assert.That( TruExpr.Parse("{not  true}").Interpret(),        Is.EqualTo(new TruBool(false)));
+            Assert.That( TruExpr.Parse("{equals true true}").Interpret(), Is.EqualTo(new TruBool(true)));
+            Assert.That( TruExpr.Parse("{equals {lambda {} true} {lambda {} true}}").Interpret(), Is.EqualTo(new TruBool(false)));
+            Assert.That( TruExpr.Parse("{let {[f {lambda {} true}]} {equals f f}}").Interpret(), Is.EqualTo(new TruBool(true)));
+            Assert.That( TruExpr.Parse("{if true true false}").Interpret(), Is.EqualTo(new TruBool(true)));
+        }
+
+        [Test]
+        public void TestLibrary() {
+            Assert.That( TruExpr.Parse("{nor  true true}").Interpret(),   Is.EqualTo(new TruBool(false)));
+            Assert.That( TruExpr.Parse("{xor  true true}").Interpret(),   Is.EqualTo(new TruBool(false)));
+            Assert.That( TruExpr.Parse("{xnor false false}").Interpret(), Is.EqualTo(new TruBool(true)));
+            Assert.That( TruExpr.Parse("{implies true false}").Interpret(), Is.EqualTo(new TruBool(false)));
+            Assert.That( TruExpr.Parse("{majority true false false}").Interpret(), Is.EqualTo(new TruBool(false)));
+        }
+    }
+}
